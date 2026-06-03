3ceb8a944fb371feff779a5ae0f7c39219ac1721
GabySuarez10 <gabysuarezcardenas11@gmail.com>
2026-06-03 12:21:17 -0500

    se agregan iframes

--- server/check_db.js
+++ server/check_db.js
@@ -13,7 +13,7 @@ async function check() {
     console.log("Migration finished successfully!");
 
     const clicsColumns = await pool.query(
-      "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'sitios'"
+      "SELECT column_name, data_type FROM information_schema.columns WHERE table_name = 'clics'"
     );
     console.log("CLICS COLUMNS:");
     console.table(clicsColumns.rows);
